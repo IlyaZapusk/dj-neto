194b9a8e80feb08d4879ec342a4e40be2d16c5e1
Илья Запускалов <ilya.zapuskalov.89@gmail.com>
2025-06-21 19:06:59 +0300

    2.2-databases-2

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,48 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Teacher, Student, Tag, Scope
+
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        super().clean()
+        main_count = 0
+        for form in self.forms:
+            if not form.cleaned_data or form.cleaned_data.get('DELETE'):
+                continue
+            if form.cleaned_data.get('is_main'):
+                main_count += 1
+        if main_count == 0:
+            raise ValidationError('Укажите один основной раздел.')
+        elif main_count > 1:
+            raise ValidationError('Можно указать только один основной раздел.')
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
+    extra = 1
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ScopeInline]
+    list_display = ('title', 'published_at')
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('name', 'subject')
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    filter_horizontal = ('teachers',)
--- 2.2-databases-2/m2m-relations/articles/urls.py
+++ 2.2-databases-2/m2m-relations/articles/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
-from articles.views import articles_list
+from articles.views import articles_list, students_list
 
 urlpatterns = [
     path('', articles_list, name='articles'),
+    path('students/', students_list, name='students'),
 ]
