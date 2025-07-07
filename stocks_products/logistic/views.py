3ae347be4edd9a2fbb2b477808f3dd5a8eb0a99b
Илья Запускалов <ilya.zapuskalov.89@gmail.com>
2025-07-07 20:37:46 +0300

    3.2-crud

---
+++ stocks_products/logistic/views.py
@@ -0,0 +1,19 @@
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+from rest_framework.pagination import LimitOffsetPagination
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'description']
+    pagination_class = LimitOffsetPagination
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['products__title', 'products__description']
+    pagination_class = LimitOffsetPagination
